d81dc41a44b7d13e14002fbeba13850805471385
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-12-11 10:31:45 +0200

    first commit

---
+++ src/main/java/ua/ucu/edu/apps/task1/MyUser.java
@@ -0,0 +1,9 @@
+package ua.ucu.edu.apps.task1;
+
+import java.time.LocalDateTime;
+
+public interface MyUser {
+    String getEmail();
+    String getCountry();
+    LocalDateTime getLastActiveTime();
+}
